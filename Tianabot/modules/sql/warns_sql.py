78b4a29c4838e274a45cdde64f11c58128bbae20
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-03-12 11:57:03 +0530

    Update warns_sql.py

--- Tianabot/modules/sql/warns_sql.py
+++ Tianabot/modules/sql/warns_sql.py
@@ -10,7 +10,7 @@ class Warns(BASE):
 
     user_id = Column(BigInteger, primary_key=True)
     chat_id = Column(String(14), primary_key=True)
-    num_warns = Column(Integer, default=0)
+    num_warns = Column(BigInteger, default=0)
     reasons = Column(postgresql.ARRAY(UnicodeText))
 
     def __init__(self, user_id, chat_id):
