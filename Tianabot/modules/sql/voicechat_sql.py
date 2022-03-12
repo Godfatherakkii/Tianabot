31ebc02c72b4dc2e332f3662595837409ffdbc5e
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-03-12 13:14:51 +0530

    Update voicechat_sql.py

--- Tianabot/modules/sql/voicechat_sql.py
+++ Tianabot/modules/sql/voicechat_sql.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText
 from Tianabot.modules.sql import BASE, SESSION
 
-
 class Talkmode(BASE):
     __tablename__ = "talkmode"
     chat_id = Column(String(14), primary_key=True)
