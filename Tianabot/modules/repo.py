84ec8fdd22764e979502a6bbde4ba4e0d3cda011
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-04-19 14:38:40 +0530

    Update repo.py

--- Tianabot/modules/repo.py
+++ Tianabot/modules/repo.py
@@ -4,16 +4,16 @@ from pyrogram import __version__ as z
 from telethon import __version__ as s
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
-from Tianabot import pbot, START_IMG, SUPPORT_CHAT
+from Tianabot import pbot, START_IMG, SUPPORT_CHAT, BOT_NAME, OWNER_USERNAME
 
 
 @pbot.on_message(filters.command("repo"))
 async def repo(_, message):
     await message.reply_photo(
         photo=START_IMG,
-        caption=f"""✨ **Hey I'm Tiana Robot** 
+        caption=f"""✨ **Hey I'm** {BOT_NAME}
 
-**Owner : [Prince](https://t.me/PrincexDevilDad)**
+**Owner : [Click Here](https://t.me/{OWNER_USERNAME})**
 **Python Version :** `{y()}`
 **Library Version :** `{o}`
 **Telethon Version :** `{s}`
